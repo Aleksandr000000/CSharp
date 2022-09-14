e9d010ad68909ff35c1cda8f69db139a0ecbed3b
Aleksandr <MrRotod@gmail.com>
2022-09-14 21:47:10 +0500

    add random

--- Examples/Example003_Sum/Program.cs
+++ Examples/Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿
-int numberA= 3;
-int numberB= 5;
+int numberA= new Random().Next(1,10); // Случайное число 0,1,2,3...9 
+Console.WriteLine(numberA);
+int numberB= new Random().Next(1,10);
+Console.WriteLine(numberB);
 int resultat= numberA+numberB;
 Console.WriteLine(resultat);
 
