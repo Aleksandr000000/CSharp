6414bb4b60f2aed26a63a5e9eca104c93ef90c0c
Александр <mrrotod@gmail.com>
2022-09-15 16:27:27 +0300

    неправильное решение задачи с собакой

---
+++ Examples/Example008_dogFriends/Program.cs
@@ -0,0 +1,27 @@
+﻿int count =0;
+int dictance = 500;
+int FirstFriendSpeed=18;
+int SecondFriendSpeed=20;
+int DogSpeed= 2;
+int Friend = 2;
+int time = 0;
+
+while (dictance > 10)
+{
+    if (Friend==1)
+    {
+    time=dictance/(FirstFriendSpeed+DogSpeed);
+    Friend=2;
+    dictance=dictance-(FirstFriendSpeed+SecondFriendSpeed)*time;
+    count=count+1;
+    }
+    if (Friend==2)
+    {
+    time=dictance/(SecondFriendSpeed+DogSpeed);
+    Friend=1;
+    dictance=dictance-(FirstFriendSpeed+SecondFriendSpeed)*time;
+    count=count+1;      
+    }
+Console.Write(count);
+}
+
